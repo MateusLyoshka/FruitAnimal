5c77fcecc539608286e9f74178839c4d25cf3bdc
MateusLyoshka <mateusbigbang@gmail.com>
2025-04-30 15:47:42 -0300

    Initial commit

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+    public float speed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        print("player");
+    }
+
+    void Move()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * speed;
+    }
+}
